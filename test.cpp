996ffd97d2044189294edd7e6817bfda51a8c7a7
Suhaas Kolli <suhaask3@illinois.edu>
2022-11-12 00:16:40 +0000

    read data from file

--- test.cpp
+++ test.cpp
@@ -1,5 +1,16 @@
 #include <iostream> 
+#include <fstream>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello World!";
+    ifstream file;
+    file.open("/workspaces/CS225/Final-Project/225-final-project/data/routes.dat");
+    string data = "";
+    while (file >> data) {
+        std::cout << data << std::endl;
+    }
+    file.close();
+    return 0;  
 }
