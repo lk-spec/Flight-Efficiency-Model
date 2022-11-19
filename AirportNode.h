7564ac72065f8c805ddac7360af17416bb4e2607
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2022-11-19 03:04:57 +0000

    added airportnode files and graph files

---
+++ AirportNode.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class AirportNode {
+public:
+    AirportNode();
+
+private:
+    
+}
--- test.cpp
+++ test.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main() {
     std::cout << "Hello World!";
     ifstream file;
-    file.open("/workspaces/CS225/Final-Project/225-final-project/data/routes.dat");
+    // may have to change the path bc it could be someone else's
+    file.open("/workspaces/CS 225/final-project/225-final-project/data/routes.dat");
     string data = "";
     while (file >> data) {
         std::cout << data << std::endl;
